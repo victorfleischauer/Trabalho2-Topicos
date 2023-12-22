5469cceea7636bf44bf6359ef2bf644c755042e9
Dartpenguin <victorfleischauer@hotmail.com>
2023-12-22 00:39:46 -0300

    settings

--- app/src/main/java/edu/ifsul/trabalho2/Settings.java
+++ app/src/main/java/edu/ifsul/trabalho2/Settings.java
@@ -6,16 +6,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Settings extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         //Inicializar variável
         bottomNavigationView = findViewById(R.id.bottom_navigation);
